701419678e1def678737810075d6bcb424275ddb
davitJabushanuri <davitjabushanuri95@gmail.com>
2022-06-04 03:28:56 +0400

    setup sanity

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import { sanityClient, urlFor } from '../sanity'
+import { Post } from '../Typings.t'
 
-const Home: NextPage = () => {
+interface Props {
+	post: [Post]
+}
+
+const Home = ({ post }: Props) => {
+	console.log(post)
 	return (
 		<div className='min-h-screen'>
 			<Head>
@@ -19,3 +25,25 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+	const query = `*[_type == "post"]{
+  _id,
+  title,
+  slug,
+  author -> {
+  name,
+  image
+ },
+ description,
+ mainImage,
+ slug
+}`
+
+	const posts = await sanityClient.fetch(query)
+	return {
+		props: {
+			posts,
+		},
+	}
+}
